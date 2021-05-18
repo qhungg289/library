90605c5c5823b709a55bb2e5ea3d8d233f5f2d0c
Hung Duong Quang <qhungg289@protonmail.com>
2021-05-18 21:25:25 +0700

    Send form value to an object

--- object.js
+++ object.js
@@ -1,15 +1,41 @@
 let myLibrary = [];
 
-function Book(title, author, pages, read) {
+document.getElementById("the-form").onsubmit = function () {
+	let readStatus;
+	if (document.getElementById("not-yet").checked) {
+		readStatus = document.getElementById("not-yet").value;
+	} else if (document.getElementById("already").checked) {
+		readStatus = document.getElementById("already").value;
+	}
+
+	addBookToLibrary(
+		document.getElementById("title").value,
+		document.getElementById("author").value,
+		readStatus
+	);
+
+	document.getElementById("title").value = null;
+	document.getElementById("author").value = null;
+	document.getElementById("not-yet").checked = false;
+	document.getElementById("already").checked = false;
+	return false;
+};
+
+function Book(title, author, read) {
 	this.title = title;
 	this.author = author;
-	this.pages = pages;
 	this.read = read;
 }
 
-function addBookToLibrary(title, author, pages, read) {
-	const newBook = new Book(title, author, pages, read);
-	myLibrary.push(newBook);
+function addBookToLibrary(title, author, read) {
+	if (title == "" || author == "" || read == undefined) {
+		alert("Enter a valid value before submit!");
+	} else {
+		const newBook = new Book(title, author, read);
+		myLibrary.push(newBook);
+	}
 }
 
-addBookToLibrary();
+function showBooksInLibrary() {
+	myLibrary.forEach((book) => console.log(book));
+}
