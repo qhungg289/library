ab3dbf18c75a8f604bf9300776be3d90c7b57000
Hung Duong Quang <qhungg289@protonmail.com>
2021-05-19 23:17:10 +0700

    Add functions to handle modal box

--- object.js
+++ object.js
@@ -1,7 +1,25 @@
 let myLibrary = [];
 let table = document.getElementById("table");
+let modal = document.getElementById("modal");
+let button = document.getElementById("button");
+let closeBtn = document.getElementById("close");
+let form = document.getElementById("the-form");
 
-document.getElementById("the-form").onsubmit = function () {
+button.onclick = function () {
+	modal.style.display = "block";
+};
+
+closeBtn.onclick = function () {
+	modal.style.display = "none";
+};
+
+window.onclick = function (event) {
+	if (event.target == modal) {
+		modal.style.display = "none";
+	}
+};
+
+form.onsubmit = function () {
 	let readStatus;
 	if (document.getElementById("not-yet").checked) {
 		readStatus = document.getElementById("not-yet").value;
@@ -19,6 +37,8 @@ document.getElementById("the-form").onsubmit = function () {
 	document.getElementById("author").value = null;
 	document.getElementById("not-yet").checked = false;
 	document.getElementById("already").checked = false;
+
+	modal.style.display = "none";
 	return false;
 };
 
@@ -30,7 +50,7 @@ function Book(title, author, read) {
 
 function addBookToLibrary(title, author, read) {
 	if (title == "" || author == "" || read == undefined) {
-		alert("Enter a valid value before submit!");
+		alert("Enter a valid information before submit!");
 	} else {
 		const newBook = new Book(title, author, read);
 		myLibrary.push(newBook);
